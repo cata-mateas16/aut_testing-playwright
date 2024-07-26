c64dfde994d27cc7517b43fac57cbf3c4d157548
cata-mateas16 <catalin.mateas@softgenius.com>
2024-07-26 16:33:26 +0300

    resolved conflicts

--- pageObjectModels/demoPage.js
+++ pageObjectModels/demoPage.js
@@ -1,19 +1,18 @@
-export class playwrightPage {
+export class DemoPage {
   constructor(page) {
     this.page = page;
 
     /* todo components */
     this.todoInsertField = page.getByPlaceholder('What needs to be done?');
     this.todoItemList = page.getByTestId('todo-title')
-    this.removeTodoItem = page.getByRole('button', { name: 'Delete' });
     this.deleteButton = page.getByRole('button', { name: 'Delete' });
-    this.nrOfItemsInTodoList = page.locator('span[data-testid="todo-count"]').nth(0).locator('strong');
+    this.nrOfItemsInTodoList = page.locator('span[data-testid="todo-count"]').first().locator('strong');
     this.checkBoxItem = page.locator('.toggle').first();
     this.activeButton = page.getByRole('link', { name: 'Active' });
     this.completedButton = page.getByRole('link', { name: 'Completed' });
     this.clearCompletedButton = page.getByRole('button', { name: 'Clear completed' });
     this.todoEditItem = page.getByTestId('todo-item');
-    this.taskTextBox = page.getByRole('textbox', { name: 'Edit' }); //this is the locator
+    this.taskTextBox = page.getByRole('textbox', { name: 'Edit' });
 
     /* footer links */
     this.createdByLink = page.getByRole('link', { name: 'Remo H. Jansen' });
@@ -26,11 +25,9 @@ export class playwrightPage {
 
   async accessPage() {
     await this.page.goto('https://demo.playwright.dev/todomvc');
-    await this.page.context().clearCookies();
   }
 
-  async insertItemsInTodoList() {
-    const todoList = ['buy coffee', 'wash dishes', 'clean shoes'];
+  async insertItemsInTodoList(todoList) {
     for (let task of todoList) {
       await this.todoInsertField.fill(task);
       await this.todoInsertField.press('Enter');
@@ -52,16 +49,17 @@ export class playwrightPage {
     await button.click();
   }
 
-  async enterEditTaskMode() {
-    await this.insertItemsInTodoList();
+  async enterFirstElementEditMode(todoList) {
+    await this.insertItemsInTodoList(todoList);
     const task = this.todoEditItem.first();
     await task.dblclick();
   }
 
   async editTask(text, key) {
     const firstTask = this.todoEditItem.first();
-    await firstTask.getByRole('textbox', { name: 'Edit' }).fill(text);
-    await firstTask.getByRole('textbox', { name: 'Edit' }).press(key); //dunno how to acces locator other ways ^
+    // await firstTask.getByRole('textbox', { name: 'Edit' }).fill(text);
+    await firstTask.locator(this.taskTextBox).fill(text);
+    await firstTask.locator(this.taskTextBox).press(key);
   }
 
   async getTodoItems() {
@@ -71,5 +69,4 @@ export class playwrightPage {
       items.push(this.todoEditItem.nth(i));
     return items;
   }
-
 }
--- tests/todo.spec.js
+++ tests/todo.spec.js
@@ -1,139 +1,139 @@
 import {test, expect} from '@playwright/test'
-import { playwrightPage } from '../pom/playwrightPage';
+import { DemoPage } from '../pageObjectModels/demoPage';
 
 test.beforeEach(async ({page}) => {
-  let testpage = new playwrightPage(page);
-  await testpage.accessPage();
+  let testPage = new DemoPage(page);
+  await testPage.accessPage();
 });
 
+const todoList = ['buy coffee', 'wash dishes', 'clean shoes'];
+const todoMvcURL = 'https://todomvc.com/';
+
 test.describe('reachability', () => {
-  test('check if URL is correct', async ({page}) => {
+  test('check that URL is correct', async ({page}) => {
     await expect(page).toHaveURL(/playwright/);
   })
   
-  test('check if title is correct', async ({page}) => {
+  test('check that title is correct', async ({page}) => {
     await expect(page).toHaveTitle(/Todo/);
   })
   
-  test('check if footer buttons redirect to correct page and have correct text', async({page}) => {
-    let testpage = new playwrightPage(page);
+  test('check that footer buttons redirect to correct page', async({page}) => {
+    let testPage = new DemoPage(page);
   
-    await expect(testpage.createdByLink).toHaveText('Remo H. Jansen');
-    await testpage.createdByLink.click();
+    await testPage.createdByLink.click();
     await expect(page).toHaveURL('https://github.com/remojansen/');
   
-    await testpage.accessPage();
+    await testPage.accessPage();
   
-    await expect(testpage.partOfLink).toHaveText('TodoMVC');
-    await testpage.partOfLink.click();
-    await expect(page).toHaveURL('https://todomvc.com/');
+    await testPage.partOfLink.click();
+    await expect(page).toHaveURL(todoMvcURL);
   })
   
-  test('check if header button redirects to correct page and have correct text', async({page}) => {
-    let testpage = new playwrightPage(page);
+  test('check that header button redirects to correct page', async({page}) => {
+    let testPage = new DemoPage(page);
   
-    await expect(testpage.headerLink).toHaveText('real TodoMVC app.');
-    await testpage.headerLink.click();
-    await expect(page).toHaveURL('https://todomvc.com/');
+    await expect(testPage.headerLink).toHaveText('real TodoMVC app.');
+    await testPage.headerLink.click();
+    await expect(page).toHaveURL(todoMvcURL);
   })
 })
 
 test.describe('add and remove tasks', () => {
-  test('check if tasks can be added in todo list', async ({page}) => {
-    let testpage = new playwrightPage(page); 
-    await testpage.insertItemsInTodoList();
+  test('check that tasks can be added in todo list', async ({page}) => {
+    let testPage = new DemoPage(page); 
+    await testPage.insertItemsInTodoList(todoList);
 
-    const todoItems = await testpage.getTodoItems();
+    const todoItems = await testPage.getTodoItems();
     for(let item of todoItems)
       await expect(item).toBeVisible();
   }); 
   
-  test('check if tasks can be deleted', async ({page}) => {
-    let testpage = new playwrightPage(page);
+  test('check that tasks can be deleted', async ({page}) => {
+    let testPage = new DemoPage(page);
   
-    await testpage.insertItemsInTodoList();
-    const itemCountBeforeDelete = await testpage.todoItemList.count();
+    await testPage.insertItemsInTodoList(todoList);
+    const itemCountBeforeDelete = await testPage.todoItemList.count();
   
-    await testpage.clickFirstDeleteButton();
+    await testPage.clickFirstDeleteButton();
   
-    const itemCountAfterDelete = await testpage.todoItemList.count();
+    const itemCountAfterDelete = await testPage.todoItemList.count();
     expect(itemCountAfterDelete).toBeLessThan(itemCountBeforeDelete);
   });
 })
 
 test.describe('check footer of todo modal', () => {
   test('check nr of items added in todoList', async({page}) => {
-    let testpage = new playwrightPage(page);
-    await testpage.insertItemsInTodoList();
+    let testPage = new DemoPage(page);
+    await testPage.insertItemsInTodoList(todoList);
     
-    const taskCounter = await testpage.todoItemList.count();
-    const itemsleft = parseInt((await testpage.nrOfItemsInTodoList.textContent()),10);
+    const taskCounter = await testPage.todoItemList.count();
+    const itemsleft = parseInt((await testPage.nrOfItemsInTodoList.textContent()),10);
     
     expect(itemsleft).toBe(taskCounter);
   });
   
-  test('check if active section shows correct tasks', async ({page}) => {
-    let testpage = new playwrightPage(page);
+  test('check that active section shows correct tasks', async ({page}) => {
+    let testPage = new DemoPage(page);
   
-    await testpage.insertItemsInTodoList();
-    const allTasksCounter = await testpage.todoItemList.count();
-    await testpage.checkBoxItem.check();
-    await testpage.activeButton.click();
-    const activeTaskCounter = await testpage.todoItemList.count();
+    await testPage.insertItemsInTodoList(todoList);
+    const allTasksCounter = await testPage.todoItemList.count();
+    await testPage.checkBoxItem.check();
+    await testPage.activeButton.click();
+    const activeTaskCounter = await testPage.todoItemList.count();
     expect(activeTaskCounter).toBeLessThan(allTasksCounter);
   })
   
-  test('check if completed section shows correct tasks', async ({page}) => {
-    let testpage = new playwrightPage(page);
+  test('check that completed section shows correct number of tasks', async ({page}) => {
+    let testPage = new DemoPage(page);
   
-    await testpage.insertItemsInTodoList();
-    await testpage.checkBoxItem.check();
-    await testpage.completedButton.click();
-    const completedTaskCounter = await testpage.todoItemList.count();
+    await testPage.insertItemsInTodoList(todoList);
+    await testPage.checkBoxItem.check();
+    await testPage.completedButton.click();
+    const completedTaskCounter = await testPage.todoItemList.count();
     expect(completedTaskCounter).toBe(1);
   })
   
-  test('check if clear completed button works', async ({page}) => {
-    let testpage = new playwrightPage(page);
+  test('check that completed item can be removed from todoList', async ({page}) => {
+    let testPage = new DemoPage(page);
   
-    await testpage.insertItemsInTodoList();
-    const tasksBefore = await testpage.todoItemList.count();
-    await testpage.checkBoxItem.check();
-    await testpage.clearCompletedButton.click();
-    const tasksAfter = await testpage.todoItemList.count();
+    await testPage.insertItemsInTodoList(todoList);
+    const tasksBefore = await testPage.todoItemList.count();
+    await testPage.checkBoxItem.check();
+    await testPage.clearCompletedButton.click();
+    const tasksAfter = await testPage.todoItemList.count();
     expect(tasksAfter).toBeLessThan(tasksBefore);
   })
 })
 
 test.describe('editability', () => {
-  test('check if task is editable', async({page}) => {
-    let testpage = new playwrightPage(page);
+  test('check that task is editable', async({page}) => {
+    let testPage = new DemoPage(page);
   
-    const task = testpage.todoEditItem.nth(0);
-    await testpage.enterEditTaskMode();
-    await expect(task.getByRole('textbox', { name: 'Edit' })).toHaveValue('buy coffee'); //dunno how to use my locator from pom here
+    const task = testPage.todoEditItem.nth(0);
+    await testPage.enterFirstElementEditMode(todoList);
+    await expect(task.getByRole('textbox', { name: 'Edit' })).toHaveValue('buy coffee');
     
-    await testpage.editTask('testing ..i. out of it','Enter');
-    await expect(testpage.todoItemList.nth(0)).toHaveText('testing ..i. out of it');
+    await testPage.editTask('testing ..i. out of it','Enter');
+    await expect(testPage.todoItemList.nth(0)).toHaveText('testing ..i. out of it');
   })
   
-  test('check if removing text from a task also remove task itself', async({page}) => {
-    let testpage = new playwrightPage(page);
+  test('check that removing text from a task also remove task itself', async({page}) => {
+    let testPage = new DemoPage(page);
   
-    await testpage.enterEditTaskMode();
-    const taskBefore = await testpage.todoItemList.count();
-    await testpage.editTask('','Enter'); 
-    const taskAfter = await testpage.todoItemList.count();
+    await testPage.enterFirstElementEditMode(todoList);
+    const taskBefore = await testPage.todoItemList.count();
+    await testPage.editTask('','Enter'); 
+    const taskAfter = await testPage.todoItemList.count();
     expect(taskAfter).toBeLessThan(taskBefore);
   })
 
-  test('check if pressing esc you quit editing', async({page}) => {
-    let testpage = new playwrightPage(page);
-    await testpage.enterEditTaskMode();
-    await testpage.editTask('testing ..i. out of it','Escape');
-    
-    const todoItems = await testpage.getTodoItems();
-    for(let item of todoItems)
-      await expect(item).toBeVisible();    
+  test('check that pressing esc quits editing mode', async({page}) => {
+    let testPage = new DemoPage(page);
+
+    await testPage.enterFirstElementEditMode(todoList);
+    await expect(testPage.todoEditItem.first()).toHaveClass(/editing/);
+    await testPage.editTask('testing ..i. out of it','Escape');
+    await expect(testPage.todoEditItem.first()).not.toHaveClass(/editing/);
   })
 })
